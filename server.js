df6bfd06bcbec746c2290c72f294dfa94a49ad8e
jakezheng <jakezheng@jakezhengs-MacBook-Pro.local>
2025-08-30 13:54:58 -0700

    Convert to normal HTML/CSS/JS website: serve static files directly, remove forced routing, add Socket.io client to public folder

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.static('public'));
 
 
 
@@ -119,24 +118,9 @@ app.use('/api/analytics', analyticsRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/exports', exportRoutes);
 
-// Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'));
-});
 
-// Serve Socket.io client library
-app.get('/socket.io/socket.io.js', (req, res) => {
-    const socketIoPath = require.resolve('socket.io-client/dist/socket.io.js');
-    res.sendFile(socketIoPath);
-});
 
 // Test route
 app.get('/test', (req, res) => {
